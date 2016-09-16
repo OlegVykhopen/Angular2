f8e4fdd581807eb0eb88bfe1726cccbb370fb042
ovyhopen <oleh.vyhopen@ukrinsoft.com>
2016-09-16 22:00:51 +0300

    initial commit

---
+++ start_app/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { DatabindingComponent } from './databinding/databinding.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app works!';
+}
---
+++ start_app/src/app/databinding/databinding.component.ts
@@ -0,0 +1,18 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-databinding',
+  templateUrl: './databinding.component.html',
+  styleUrls: ['./databinding.component.css']
+})
+export class DatabindingComponent{
+
+  stringInterpolation: String;
+  numberInterpolation: Number;
+
+  constructor() {
+    this.stringInterpolation = "This is string interpolation";
+    this.numberInterpolation = 2;
+  }
+
+}
